7aa95677e9697346558223991d03b09ff88de951
Bryan Escobedo <bryan.ae95@gmail.com>
2022-05-18 15:32:49 -0500

    map api configured, content added

--- src/app/Modules/location/location.component.ts
+++ src/app/Modules/location/location.component.ts
@@ -8,31 +8,23 @@ import { MapInfoWindow, MapMarker, GoogleMap } from '@angular/google-maps';
 })
 
 export class LocationComponent  {
-  map: any;
-  @ViewChild('map') mapElement: any;
-  lat = 43.879078;
-  lng = -103.4615581;
-  markers = [
-    { lat: 22.33159, lng: 105.63233 },
-    { lat: 7.92658, lng: -12.05228 },
-    { lat: 48.75606, lng: -118.859 },
-    { lat: 5.19334, lng: -67.03352 },
-    { lat: 12.09407, lng: 26.31618 },
-    { lat: 47.92393, lng: 78.58339 }
-  ];
+  
+  mapOptions: google.maps.MapOptions = {
+    center: { lat: 26.224996554132048, lng: -98.22633191584079 },
+    zoom : 18
+ }
+ 
+ marker = {
+    position: { lat:  26.22536939191419,  lng: -98.22692232119
+    },
+ }
+
+
   constructor() { }
   ngAfterViewInit(): void {
-    const mapProperties = {
-      center: new google.maps.LatLng(this.lat, this.lng),
-      zoom: 2,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    this.map = new google.maps.Map(this.mapElement.nativeElement, mapProperties);
-    this.markers.forEach(location => {
-      var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(location.lat, location.lng),
-        map: this.map
-      });
-    });
+   
+    
   }
+ 
+  
 }
